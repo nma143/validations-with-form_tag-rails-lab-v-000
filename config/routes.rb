46edc216b1572260639a2700a34d24a29b9350ab
Nikki A <nikki.apostolakis@gmail.com>
2019-01-28 15:37:16 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,6 @@ Rails.application.routes.draw do
 
   resources :authors
   resources :posts
-  
-  # get "/authors/new", to: "authors#new", as: "new_author"
-  # get "/authors/:id", to: "authors#show", as: "author"
-  # post "/authors", to: "authors#create"
-  #
-  # get "/posts/:id", to: "posts#show", as: "post"
-  # get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
-  # patch "/posts/:id", to: "posts#update"
-
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
